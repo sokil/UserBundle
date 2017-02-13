5705b7d2782b52b5d039923c3a087afcca5a8640
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-02-13 23:33:39 +0200

    user command

--- src/Command/CreateUserCommand.php
+++ src/Command/CreateUserCommand.php
@@ -6,13 +6,20 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class UserCommand extends Command
+class CreateUserCommand extends Command
 {
     protected function execute(
         InputInterface $input,
         OutputInterface $output
     ) {
-    {
         throw new \Exception("Command not implemented");
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('user:create')
+            ->setDescription('Create user')
+            ->setHelp('Create user');
     }
 }
