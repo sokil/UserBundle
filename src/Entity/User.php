4443d83f10e4f654eb21be3ad1e89f256e6328c0
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-07-07 00:13:32 +0300

    email is unique

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -39,7 +39,7 @@ class User implements AdvancedUserInterface, \Serializable
 
     /**
      * @var string
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     protected $email;
 
